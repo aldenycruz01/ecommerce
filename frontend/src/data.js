84537bf2550e18803398952b56a709a9542941a7
aldenycruz01 <aldenysix@gmail.com>
2022-06-21 13:20:30 -0300

    ECOMMERCE - IMAGENS

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim',
+      slug: 'nike-slim',
+      category: 'blusa',
+      image: '/imagens/p1.jpg',
+      price: 140,
+      countInStock: 20,
+      brand: 'nike',
+      rating: 4.6,
+      numRewies: 10,
+      description: 'Blusa de alta qualidade !',
+    },
+    {
+      name: 'Adidas Slim',
+      slug: 'adidas-slim',
+      category: 'blusa',
+      image: '/imagens/p2.jpg',
+      price: 90,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 3.6,
+      numRewies: 10,
+      description: 'Blusa de alta qualidade !',
+    },
+    {
+      name: 'Puma Slim',
+      slug: 'Puma-slim',
+      category: 'blusa',
+      image: '/imagens/p3.jpg',
+      price: 140,
+      countInStock: 20,
+      brand: 'Puma',
+      rating: 4.6,
+      numRewies: 10,
+      description: 'Blusa de alta qualidade !',
+    },
+  ],
+};
+
+export default data;
