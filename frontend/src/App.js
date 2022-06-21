92b679d5c9584db6c391b33cd4be1590dce3145f
aldenycruz01 <aldenysix@gmail.com>
2022-06-21 00:21:37 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ecommerce</a>
+      </header>
+      <main>Lista de Produtos</main>
+    </div>
+  );
+}
+
+export default App;
